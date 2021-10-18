591537044162a7f344a736ab27363f404e3d8a37
Fernando Rocha <fhrlobacz@gmail.com>
2021-10-18 16:22:18 -0300

    corrigido o host smtp

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import smtplib
 import secrets
 
 from_addr = secrets.sender
-to_addrs = [secrets.receiver]
+to_addrs = secrets.sender
 msg = """Essa mensagem foi enviada por um script em python.
 Utilizando a biblioteca smtplib.
 """
 
 try:
-    s = smtplib.SMTP('localhost')
+    s = smtplib.SMTP(secrets.smtp_host,587)
     s.login(secrets.sender, secrets.sender_password)
     s.sendmail(from_addr, to_addrs, msg)
     s.quit()
